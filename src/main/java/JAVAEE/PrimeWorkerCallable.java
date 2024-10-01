c39833f4fb3bc34023bb11caa2e1963bae99ba85
amir.yosef <amir.yosef@hiast.edu.sy>
2024-10-01 20:33:35 +0300

    add Worker class to split work across threads

---
+++ src/main/java/JAVAEE/PrimeWorkerCallable.java
@@ -0,0 +1,18 @@
+package JAVAEE;
+
+import java.util.List;
+import java.util.concurrent.Callable;
+
+public class PrimeWorkerCallable implements Callable<List<Integer>> {
+    private final PrimeAlgorithm algorithm;
+
+    public PrimeWorkerCallable(int low, int high, PrimeAlgorithm algorithm) {
+        this.algorithm = algorithm;
+    }
+
+    @Override
+    public List<Integer> call() {
+        return algorithm.findPrimes();
+    }
+}
+
